5c4da88641c0eff528c37585fc4d6d35889ebbb7
Bala <rbalavignesh06@gmail.com>
2026-07-04 21:29:28 +0530

    update: replace resume google drive link again

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -4,7 +4,7 @@ export function ResumeButton() {
   return (
     <div className="fixed right-0 top-1/2 -translate-y-1/2 z-50 hidden md:flex">
       <a
-        href="https://drive.google.com/file/d/1oERQDI5s28-827fnoUL49KrTqK5nXpsp/view?usp=sharing"
+        href="https://drive.google.com/file/d/1pkixxG-gi4FGq0moxAl_Yi8TynglSnHH/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
         className="relative flex items-center gap-2 bg-black/30 backdrop-blur-md border border-r-0 border-white/10 p-2 rounded-l-xl shadow-lg overflow-hidden group hover:border-amber-500/50 transition-colors duration-300"
